ce2eb4c793a68ee31f8dbcf142232afb542c7cf2
[AaronCruzH] <[fraronzez@gmail.com]>
2022-05-20 18:28:42 -0500

    Correccíon UT

--- test/lib/services/ExplorerService.test.js
+++ test/lib/services/ExplorerService.test.js
@@ -11,8 +11,8 @@ describe("Test Unit para ExplorerService",()=>{
         const explorersAmount=ExplorerService.getAmountOfExplorersByMission(explorers, "node");
         const explorersNames=ExplorerService.getExplorersUsernamesByMission(explorers, "node");
 
-        expect(explorersMission).toBeUndefined()
+        expect(explorersMission).not.toBeUndefined()
         expect(explorersAmount).toBe(10)
-        expect(explorersNames).toBeUndefined()
+        expect(explorersNames).not.toBeUndefined()
     })
 })
